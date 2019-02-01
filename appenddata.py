8029d26f5652e9b6343e3eb0054cce5510bf0479
blackpanther409 <41005814+blackpanther409@users.noreply.github.com>
2019-02-01 14:08:26 +0530

    Create appenddata.py

---
+++ appenddata.py
@@ -0,0 +1,3 @@
+with open('test.txt','a')as local_toadd:
+    local_toadd.write('LETS LEARN PYTHON')
+#appending is adding, the above code adds the given data to already existing data of the file test.txt
