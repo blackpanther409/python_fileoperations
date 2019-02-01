010572f4b095c0079a8e29b7162ed9167eaeb110
blackpanther409 <41005814+blackpanther409@users.noreply.github.com>
2019-02-01 14:04:36 +0530

    Update writedata.py

--- writedata.py
+++ writedata.py
@@ -1,3 +1,4 @@
 #writing to a file named 'test.txt' using with
-with open('test.txt','w+')as local_towrite:
+with open('test.txt','w')as local_towrite:
     local_towrite.write('I LOVE CODING IN PYTHON')
+#becareful about the giving the file name, you have to give the path clearly
