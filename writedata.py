daf0d63bd7f00f0d032f0e670582a95f3593556f
blackpanther409 <41005814+blackpanther409@users.noreply.github.com>
2019-02-01 14:09:48 +0530

    Update writedata.py

--- writedata.py
+++ writedata.py
@@ -2,3 +2,4 @@
 with open('test.txt','w')as local_towrite:
     local_towrite.write('I LOVE CODING IN PYTHON')
 #becareful about the giving the file name, you have to give the path clearly
+#writing to a file means rewriting the previous data of the file with the given data
