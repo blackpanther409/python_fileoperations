90b795ce8bbb38fb8c9a0bd14d81f5fdda4c352b
blackpanther409 <41005814+blackpanther409@users.noreply.github.com>
2019-02-05 19:43:02 +0530

    Update readbetween.py

--- readbetween.py
+++ readbetween.py
@@ -1,6 +1,11 @@
+#getting the line no. to be displayed by user
 num=int(input("Enter the line to be displayed:"))
+
+#opening the file and enumerating(line no.,line) the lines in it
 with open("C:\\Users\\sahithi nandyala\\Documents\\here to succeeed\\PYTHON\\test.txt") as fp:
     for i, line in enumerate(fp):
         if i+1 == num:
             print(line)
-         
+#tips:            
+#be careful about giving the file address correctly
+            
