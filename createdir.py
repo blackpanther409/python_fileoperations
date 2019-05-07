f36f308a6d564471cfcb7312bcb8e71d8792f83f
blackpanther409 <41005814+blackpanther409@users.noreply.github.com>
2019-05-07 14:22:23 +0530

    Update createdir.py
    we can create a new directory or even create a subdirectory of an existing directory in current directory
    Go through the comments for clarity

--- createdir.py
+++ createdir.py
@@ -11,3 +11,12 @@ try:
     os.mkdir(newdir)
 except FileExistsError as exc:
     print(exc)
+
+'''
+To create subdirectory we need to enter the tree of dirs that we want to create
+for example
+on entering
+parentdir/childdir
+this creates childdir inside parentdir which is already present in the current dir
+so here we created a subdirectory(childdir here)
+'''
